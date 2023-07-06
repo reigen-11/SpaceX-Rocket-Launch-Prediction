231bdd3285696bb046ec409cd72e5d92609bd59b
Garo-U <adityadurugkar07@gmail.com>
2023-07-06 21:45:33 +0530

    SpaceX Public API

---
+++ src/data/api.py
@@ -0,0 +1,8 @@
+#SpaceX public API
+
+SPACEX_URL = "https://api.spacexdata.com/v4/launches/past"
+
+launch_pad = 'https://api.spacexdata.com/v4/launchpads/'
+rockets = 'https://api.spacexdata.com/v4/rockets/'
+payloads = 'https://api.spacexdata.com/v4/payloads/'
+cores = 'https://api.spacexdata.com/v4/cores/'
