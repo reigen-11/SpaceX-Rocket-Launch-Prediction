d5708a968f67815d030f73d9e88689cdf757484f
Garo-U <adityadurugkar07@gmail.com>
2023-07-07 21:49:14 +0530

    logger for logs

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+logs_path = os.path.join('/home/aditya/All Github/SpaceX/logs', LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
@@ -14,4 +14,4 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
+logging.info("Init Log")
